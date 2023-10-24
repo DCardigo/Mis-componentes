d73f775c4a48e690ce7e84e9f53afed52e1bbe61
Daniel Carrión Martínez <danicm89@gmail.com>
2023-10-24 10:02:49 +0000

    botones colocadas

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,7 +6,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
+		<div className="box">
+<button  className="ico star mx-auto"><i class="far fa-star"></i></button >
+			<div className="ico star mx-auto"><i class="far fa-star"></i></div>
+			<div className="ico list mx-auto"><i class="fas fa-list-ul"></i></div>
+			<div className="ico step mx-auto">step</div>
 			
 		</div>
 	);
