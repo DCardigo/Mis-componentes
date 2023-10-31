e2eff64ec82d390b998415535dd1d1d6ae7d9584
Daniel Carrión Martínez <danicm89@gmail.com>
2023-10-31 11:25:35 +0000

    pruebo varias opciones

--- src/js/component/star.js
+++ src/js/component/star.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { MDBRating, MDBRatingElement } from 'mdb-react-ui-kit';
 
 
 
@@ -15,16 +16,23 @@ const Star = () => {
 	const [count4, setCount4] = useState(false)
 	const [count5, setCount5] = useState(false)
 
-	const handlerClick5 = () =>{
+
+	const handlerClick5 = () => {
 		setCount5(!count5);
 		console.log(count5);
-		if (count5  === true){
+		if (count5 === true) {
+
 			setStar1()
 			setStar2()
 			setStar3()
 			setStar4()
 			setStar5()
-		} else{
+		} else {
+			setCount1(!count1);
+			setCount2(!count2);
+			setCount3(!count3);
+			setCount4(!count4);
+
 			setStar1('rgb(255, 217, 0)')
 			setStar2('rgb(255, 217, 0)')
 			setStar3('rgb(255, 217, 0)')
@@ -33,76 +41,94 @@ const Star = () => {
 		}
 	}
 
-	const handlerClick1 = () =>{
+	const handlerClick1 = () => {
 		console.log(count1);
 		setCount1(!count1);
-		if (count1  === true){
+		if (count1 === true) {
 			setStar1()
-		} else{
+		} else {
+
 			setStar1('rgb(255, 217, 0)');
 		}
-		if (count2  === true || count3  === true || count4  === true || count5  === true){
+		// || count3 === true || count4 === true || count5 === true
+		if (count2 === true) {
+
 			setStar2()
 			setStar3()
 			setStar4()
 			setStar5()
 		}
 	}
-	const handlerClick2 = () =>{
+	const handlerClick2 = () => {
 		console.log(count2);
 		setCount2(!count2);
-		if (count2  === true){
+		if (count2 === true) {
 			setStar1()
 			setStar2()
-		} else{
+		} else {
+			setCount1(!count1);
+
 			setStar1('rgb(255, 217, 0)')
 			setStar2('rgb(255, 217, 0)')
 		}
-		if (count3  === true || count4  === true || count5  === true){
-			
+		// || count4 === true || count5 === true
+		if (count3 === true) {
+
+
 			setStar3()
 			setStar4()
 			setStar5()
 		}
 	}
-	const handlerClick3 = () =>{
+	const handlerClick3 = () => {
 		console.log(count3);
 		setCount3(!count3);
-		if (count3  === true){
+		if (count3 === true) {
 			setStar1()
 			setStar2()
 			setStar3()
-		} else{
+		} else {
+			setCount1(!count1);
+			setCount2(!count2);
+
 			setStar1('rgb(255, 217, 0)')
 			setStar2('rgb(255, 217, 0)')
 			setStar3('rgb(255, 217, 0)')
 		}
-		if (count4  === true || count5  === true){
-			
+		// || count5 === true
+		if (count4 === true) {
+
+
 			setStar4()
 			setStar5()
 		}
 	}
-	const handlerClick4 = () =>{
+	const handlerClick4 = () => {
 		console.log(count4);
 		setCount4(!count4);
-		if (count4 === true){
+		if (count4 === true) {
+
 			setStar1()
 			setStar2()
 			setStar3()
 			setStar4()
-		} else{
+		} else {
+			setCount1(!count1);
+			setCount2(!count2);
+			setCount3(!count3);
+
 			setStar1('rgb(255, 217, 0)')
 			setStar2('rgb(255, 217, 0)')
 			setStar3('rgb(255, 217, 0)')
 			setStar4('rgb(255, 217, 0)')
 		}
-		if (count5  === true){
-			
+		if (count5 === true) {
+
+
 			setStar5()
 		}
 	}
-	
+
 
 	// const [color, setColor] = useState('rgba(210, 210, 210, 0.61)');
 	// const [color1, setColor1] = useState('rgba(210, 210, 210, 0.61)');
@@ -136,7 +162,11 @@ const Star = () => {
 	// 	if (enter1 % 2 === 1){setColor1('rgba(210, 210, 210, 0.61)')&& setStar2(!star2)}
 	// };
 
+
+
 	return (
+
+
 		<div className="box">
 
 			<div className="text-center " id="star">
@@ -145,15 +175,19 @@ const Star = () => {
 					<i id="estrella" className="fas fa-star" style={{ color: star1 }} onClick={handlerClick1}></i>
 					<i id="estrella" className="fas fa-star" style={{ color: star2 }} onClick={handlerClick2}></i>
 					<i id="estrella" className="fas fa-star" style={{ color: star3 }} onClick={handlerClick3}></i>
-					<i id="estrella" className="fas fa-star" style={{ color: star4 }} onClick={handlerClick4}></i>
-					<i id="estrella" className="fas fa-star" style={{ color: star5 }} onClick={handlerClick5}></i>
-					
+					<i id="estrella" className="fas fa-star" style={{ color: star4 }} onClick={handlerClick4} ></i>
+					<i id="estrella" className="fas fa-star" style={{ color: star5 }} onClick={handlerClick5} ></i>
+
 				</div>
+
+
 
 				<p>Este es el contenido del modal</p>
 			</div>
 
 		</div>
+
+
 	);
 };
 
