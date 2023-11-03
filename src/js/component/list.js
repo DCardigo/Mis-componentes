9f48942efde9e796f378cde6f2c3708669c6e86d
Daniel Carrión Martínez <danicm89@gmail.com>
2023-11-03 15:41:38 +0000

    sigo con la l´ógica de la lista

--- src/js/component/list.js
+++ src/js/component/list.js
@@ -10,6 +10,14 @@ const List = () => {
 		setItems([...items, newItem]);
 	};
 
+	//   ELIMINAR ELEMENTO
+
+	const removeItem = (tarea, i) => {
+		const updatedItems = items.filter((item) => item != tarea);
+		setItems(updatedItems);
+
+	};
+
 
 	return (
 		<div className="text-center" id="lista">
@@ -20,15 +28,20 @@ const List = () => {
 				{/* Dropdown */}
 				<div className="dropdown col-8 text-center">
 					{items.map((item, index) => (
+						
 						<div key={index} onClick={() => addItem(item)} id="seleccion" className="rounded">
 							{items.length}. {item}
+							<button type="button" onClick={() => removeItem(item, i)} className="btn-close float-end px-2 py-0" aria-label="Close"></button>
 						</div>
 					))}
 					<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
 						Dropdown button
 					</button>
 					<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-						<li><a className="dropdown-item" onClick={() => addItem('React')} href="#">React</a></li>
+						<li>
+							<p className="dropdown-item" onClick={() => addItem('React')} href="#">React</p>
+							
+						</li>
 						<li><a className="dropdown-item" href="#">Another action</a></li>
 						<li><a className="dropdown-item" href="#">Something else here</a></li>
 					</ul>
